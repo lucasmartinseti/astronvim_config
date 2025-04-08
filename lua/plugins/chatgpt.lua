1cbeeded94923ebcb7d1683c4e63d0082385a770
Lucas Martins <lucas.martins@masonweb.com.br>
2025-04-08 12:46:45 -0300

    Add config actions for plugin Jackmort/ChatGPT.

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -6,10 +6,10 @@ return {
         config = function()
             local home = vim.fn.expand("$HOME")
             require("chatgpt").setup({
-                api_key_cmd = "gpg --decrypt " .. home .. "/.config/openai/secret.txt.gpg",
+              api_key_cmd = "gpg --decrypt " .. home .. "/.config/openai/secret.txt.gpg",
                 model = "gpt-4",
-                keymaps = {
-                  submit = "<C-s>",
+                actions_paths = {
+                  vim.fn.stdpath("config") .. "/chatgpt_actions.json"
                 },
             })
         end,
